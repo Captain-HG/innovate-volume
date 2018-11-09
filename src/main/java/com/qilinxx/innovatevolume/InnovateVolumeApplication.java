542bf1e888f5e6d5a12b25b8afdfefad5a8fc2d3
lzc <1436899024@qq.com>
2018-11-09 14:34:05 +0800

    Initial commit

---
+++ src/main/java/com/qilinxx/innovatevolume/InnovateVolumeApplication.java
@@ -0,0 +1,12 @@
+package com.qilinxx.innovatevolume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovateVolumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnovateVolumeApplication.class, args);
+    }
+}
