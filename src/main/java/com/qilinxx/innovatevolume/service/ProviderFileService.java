55061ef4dae59c29f91d80356da83c9d60acb83a
lzc <1436899024@qq.com>
2018-11-20 11:02:59 +0800

    admin的提供商的大部分功能，差查询合同查询卷

---
+++ src/main/java/com/qilinxx/innovatevolume/service/ProviderFileService.java
@@ -0,0 +1,15 @@
+package com.qilinxx.innovatevolume.service;
+
+import com.qilinxx.innovatevolume.domain.model.ProviderFile;
+import jdk.nashorn.internal.ir.IdentNode;
+
+import java.util.List;
+
+public interface ProviderFileService {
+    /** 根据商家id，查询所有文件*/
+    List<ProviderFile> selectAllByProviderId(String id);
+    /** 使某个文件失效*/
+    void noExamineFile(Integer id);
+    /** 启用某个文件*/
+    void startFile(Integer id);
+}
--- src/main/java/com/qilinxx/innovatevolume/service/ProviderService.java
+++ src/main/java/com/qilinxx/innovatevolume/service/ProviderService.java
@@ -8,11 +8,15 @@ public interface ProviderService {
     /**查询所有的提供商 */
     List<Provider> selectAll();
     /** 根据id查询提供商的信息*/
-    Provider selectById(String code);
+    Provider selectById(String  id);
      /** 重新审核该提供商，设置状态为0*/
     String examineProvider(String id);
     /** 审核通过该商家*/
     String startProvider(String id);
     /** 审核未通过*/
     String noExamineProvider(String id);
+    /** 检验商家码是否已经被使用*/
+    String ifCodeUse(String code);
+    /** 更新商家*/
+    void updateProvider(Provider provider);
 }
